634b2facc64db04f715cd0c59ba555b23b9b21a5
SujanPd-bajgain2920 <sujanprasadbajgain731@gmail.com>
2026-05-31 14:13:36 +0545

    login command handler done

---
+++ APIproject/Application/Features/Auth/Commands/LoginCommand.cs
@@ -0,0 +1,12 @@
+﻿using APIproject.Application.DTOs;
+using MediatR;
+
+namespace APIproject.Application.Features.Auth.Commands
+{
+    public class LoginCommand : IRequest<UserResponseDto?>
+    {
+        public string EmailAddress { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+    }
+}
---
+++ APIproject/Application/Features/Auth/Commands/LoginCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using APIproject.Application.DTOs;
+using APIproject.Domain.Interfaces;
+using Microsoft.AspNetCore.DataProtection;
+using MediatR;
+
+namespace APIproject.Application.Features.Auth.Commands
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, UserResponseDto?>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IDataProtector _protector;
+
+        public LoginCommandHandler(IUserRepository userRepository,
+            IDataProtectionProvider dataProtectionProvider)
+        {
+            _userRepository = userRepository;
+            _protector = dataProtectionProvider.CreateProtector("UserLogin");
+
+        }
+
+        public async Task<UserResponseDto?> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.EmailAddress);
+            if (user == null)
+            {
+                return null; // User not found
+            }
+
+            var decryptedPassword = _protector.Unprotect(user.UserPassword);
+            if (decryptedPassword != request.Password)
+                return null;
+
+            return new UserResponseDto
+            {
+                UserId = user.UserId,
+                FirstName = user.FirstName,
+                EmailAddress = user.EmailAddress,
+                UserRole = user.UserRole,
+                UserPhoto = user.UserPhoto
+            };
+        }
+    }
+}
