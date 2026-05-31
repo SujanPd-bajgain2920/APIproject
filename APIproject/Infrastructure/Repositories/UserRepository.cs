d3d032231e14af75bc1720034ea73a2f6741fb81
SujanPd-bajgain2920 <sujanprasadbajgain731@gmail.com>
2026-05-31 18:35:07 +0545

    user repository done

---
+++ APIproject/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using APIproject.Domain.Entities.Models;
+using APIproject.Domain.Interfaces;
+using APIproject.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIproject.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly FypContext _context;
+
+        public UserRepository(FypContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserList?> GetByEmailAsync(string email)
+        {
+            return await _context.UserLists
+                .FirstOrDefaultAsync
+                (x => x.EmailAddress.ToUpper() == email.ToUpper());
+        }
+
+        public async Task<UserList?> GetByIdAsync(short id)
+        {
+            return await _context.UserLists
+                .FirstOrDefaultAsync(x => x.UserId == id);
+        }
+
+        public async Task<bool> EmailExistsAsync(string email)
+        {
+            return await _context.UserLists
+                .AnyAsync(x => x.EmailAddress == email);
+        }
+
+        public async Task<short> GetNextUserIdAsync()
+        {
+            return _context.UserLists.Any()
+                ? (short)(_context.UserLists.Max(x => x.UserId) + 1)
+                : (short)1;
+        }
+
+        public async Task AddAsync(UserList user)
+        {
+            _context.UserLists.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(UserList user)
+        {
+            _context.UserLists.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
